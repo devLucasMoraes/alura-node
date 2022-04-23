b73523d6baf19a2f603536f0d315608490d3632c
devLucasMoraes <devlucasmoraes@gmail.com>
2022-04-23 15:19:19 -0300

    criado rotas

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const http = require('http')
 const port = 3000
 
+const rotas = {
+    '/': 'Curso de node',
+    '/livros': 'Entrei na pag de livros',
+    '/autores': 'listagem de autores'
+}
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'})
-    res.end('Curso de Node')
+    res.end(rotas[req.url])
 })
 
 
