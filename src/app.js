5e5aa8cc2a33aaf2ffbf243edab27b2c98f6a639
devLucasMoraes <devlucasmoraes@gmail.com>
2022-04-23 21:45:39 -0300

    criado metodo post para intregar com o postman no formato json

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 
 const app = express()
 
+app.use(express.json())
+
 const livros = [
     {id:1, "titulo": "Senhor dos Aneis"},
     {id:2, "titulo": "O Hobiit"}
@@ -12,6 +14,11 @@ app.get('/', (req, res) => {
 })
 app.get('/livros', (req, res) => {
     res.status(200).json(livros)
+})
+
+app.post('/livros', (req, res) => {
+    livros.push(req.body)
+    res.status(201).send('Livro cadastrado com sucesso')
 })
 
 export default app
