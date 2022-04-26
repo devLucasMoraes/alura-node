43b33d56f428ed11d5460ad8a4f7241bfa2b6198
devLucasMoraes <devlucasmoraes@gmail.com>
2022-04-25 22:08:09 -0300

    correcao do bug

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb+srv://trismegistus:123@alura-node.yl9j0.mongodb.net/myFirstDatabase?alura-node")
+mongoose.connect("mongodb+srv://trismegistus:123@alura-node.yl9j0.mongodb.net/alura-node")
 
 let db = mongoose.connection
 
