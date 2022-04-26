63ea381b7dc9a9b34fb27f70f972fd3340f54740
devLucasMoraes <devlucasmoraes@gmail.com>
2022-04-25 22:20:41 -0300

    criado conexao com mongoDB com mongoose

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import db from "./config/dbConnect.js"
+import livros from "../models/Livro.js"
 
 db.on("error", console.log.bind(console, 'Erro de conexão'))
 db.once("open", () => {
@@ -10,10 +11,10 @@ const app = express()
 
 app.use(express.json())
 
-const livros = [
+/* const livros = [
     {id:1, "titulo": "Senhor dos Aneis"},
     {id:2, "titulo": "O Hobiit"}
-]
+] */
 
 app.get('/livros/:id', (req, res) => {
     let index = buscaLivro(req.params.id)
@@ -23,7 +24,9 @@ app.get('/', (req, res) => {
     res.status(200).send('Curso de Node')
 })
 app.get('/livros', (req, res) => {
-    res.status(200).json(livros)
+    livros.find((err, livros) => {
+        res.status(200).json(livros)
+    })
 })
 
 app.post('/livros', (req, res) => {
